0af95ee2dc32a53c2456cabae7e20c009565495f
Samriddha-Bhattarai <samriddha.cs24@bmsce.ac.in>
2025-11-04 11:23:18 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+abstract class shape{
+    int a;
+    int b;
+    abstract void printarea();
+}
+class rectangle extends shape{
+    rectangle (int x, int y){
+        a=x;
+        b=y;
+    }
+    void printarea(){
+        System.out.println("Area of rectangle ="+(a*b));
+    }
+}
+class triangle extends shape{
+    triangle (int x, int y){
+        a=x;
+        b=y;
+    }
+    void printarea(){
+        System.out.println("Area of triangle="+(a*b*0.5));
+    }
+}
+class circle extends shape{
+    circle(int r){
+        a=r;
+    }
+    void printarea(){
+        System.out.println("Area of circle=" + (Math.PI*a*a));
+    }
+}
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        shape s;
+        System.out.println("Enter the dimensions of the rectangle: ");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        s = new rectangle(a,b);
+        s.printarea();
+
+        System.out.println("Enter the dimensions of triangle: ");
+        int c= sc.nextInt();
+        int d= sc.nextInt();
+        s = new triangle(c,d);
+        s.printarea();
+         System.out.println("Enter the dimensions of circle: ");
+          int o= sc.nextInt();
+        s = new circle(o);
+        s.printarea();
+    }
+}
