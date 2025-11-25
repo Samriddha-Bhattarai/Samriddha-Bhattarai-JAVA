ada944e3b259aa88a2db0107f9658eb58df9a258
Samriddha-Bhattarai <samriddha.cs24@bmsce.ac.in>
2025-11-25 11:29:49 +0530

    Update and rename Lab7.java to lab7.java

--- lab7.java
+++ lab7.java
@@ -35,7 +35,7 @@ class Son extends Father {
 }
 
 
-public class Lab7 {
+public class lab7 {
     public static void main(String[] args) {
         try {
           
@@ -50,3 +50,4 @@ public class Lab7 {
     }
 
 }
+
